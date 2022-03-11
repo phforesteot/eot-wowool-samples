488e285a1b71ab9bf4c4af6c15e70538829a3f61
Philippe forest <phforest@eyeontext.com>
2022-03-11 12:23:22 +0100

    pipeline samples

---
+++ eot/wowool/samples/pipelines.py
@@ -0,0 +1,15 @@
+#  Copyright (c) 2020 EyeOnText, All Rights Reserved.
+#  NOTICE:  All information contained herein is, and remains the property of EyeOnText.
+
+from eot.wowool.native import PipeLine
+from eot.wowool.error import Error
+
+try:
+    # pipeline = PipeLine("english,entity,dates")
+    # more resent version 2.4.4>= you can add applications, like geotags
+    pipeline = PipeLine("english,entity,dates")
+    doc = pipeline("John Smith was in London on the 3/11/2020. He took a cab yesterday to the Victoria station in London")
+    print('-' *80)
+    print(doc)
+except Error as ex:
+    print("Exception:", ex)
