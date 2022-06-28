6f5fc1f82894b74f53d05ff344c30a0290e7bd35
Philippe forest <phforest@eyeontext.com>
2022-06-28 11:49:57 +0200

    added topics pipeline

--- eot/wowool/samples/pipelines_topics.py
+++ eot/wowool/samples/pipelines_topics.py
@@ -6,12 +6,11 @@ from eot.wowool.error import Error
 import json
 
 try:
-    # more resent version 2.4.4>= you can add applications, like sentiments
-    pipeline_description = "english,entity,sentiment,sentiments.app"
+    pipeline_description = "english,entity,topics.app"
     print(f"{pipeline_description=}")
     pipeline = PipeLine(pipeline_description)
-    doc = pipeline("John Smith and Mary Janssens are nice people. He is liked by a lot of people. But she hated him.")
+    doc = pipeline("NFT scams, toxic mines and lost life savings: the cryptocurrency dream is fading fast")
     print('-' *80)
-    print(json.dumps(doc.results("eot_sentiments"), indent=2))
+    print(json.dumps(doc.results("eot_topics"), indent=2))
 except Error as ex:
     print("Exception:", ex)
